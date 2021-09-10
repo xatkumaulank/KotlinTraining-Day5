eefceaddaa8838010d37942693df1ea6c777256a
xatkumaulank <70027310+xatkumaulank@users.noreply.github.com>
2021-09-10 16:30:56 +0700

    interface

--- src/main/kotlin/Interface_.kt
+++ src/main/kotlin/Interface_.kt
@@ -1,2 +1,24 @@
+interface InterfaceProperties {
+    val a : Int
+    val b : String
+        get() = "Hello"
+}
 
+interface InterfaceMethods {
+    fun description()
+}
 
+class MultipleInterface : InterfaceProperties, InterfaceMethods {
+    override val a : Int
+        get() = 50
+
+    override fun description()
+    {
+        println("Multiple Interfaces implemented with $a and $b")
+    }
+}
+fun main()
+{
+    val obj = MultipleInterface()
+    obj.description()
+}
